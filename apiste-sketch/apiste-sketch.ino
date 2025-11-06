c1fadbe18c4b56ce5d83aa46146b91109eeeaf5a
Petr Stepanov <stepanovps@gmail.com>
2025-11-05 20:05:56 -0800

    Update apiste-sketch.ino

--- apiste-sketch/apiste-sketch.ino
+++ apiste-sketch/apiste-sketch.ino
@@ -12,6 +12,7 @@
 #define HAS_DHT11        // Temperature and humidity sensor
 #define HAS_WTR
 #define HAS_BUZZ
+// #define HAS_SERIAL
 
 #ifdef HAS_ETHERNET
   // Add library "ArduinoModbus" by Arduino
@@ -61,13 +62,14 @@
 #endif
 
 #ifdef HAS_LCD
-  void printLCD(char string[], bool clear = false){
+  void printLCD(char string[], bool clear = true){
     if (clear) lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(string);
   }
 
   void printLCD(char string1[], char string2[]){
+    lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(strlen(string1) != 0 ? string1 : "                ");
     lcd.setCursor(0, 1);
@@ -95,67 +97,85 @@ void setup() {
     int status;
     status = lcd.begin(LCD_COLS, LCD_ROWS);
     if (status){
-      Serial.println("LCD Error");
-      Serial.println(status);
+      #ifdef HAS_SERIAL
+        Serial.println("LCD Error");
+        Serial.println(status);
+      #endif
       hd44780::fatalError(status); // does not return
     }
 	  // Print a message to the LCD
-    Serial.println("LCD OK");
+    #ifdef HAS_SERIAL
+      Serial.println("LCD OK");
+    #endif
 	  lcd.print("Initializing...");
   #endif
 
   // Initialize serial communication with the Arduino (TTL)
-  Serial.begin(9600);
-  while (!Serial.available()) {
-     ; // wait for serial port to connect. Needed for native USB port only
-  }
+  #ifdef HAS_SERIAL
+    Serial.begin(9600);
+    while (!Serial.available()) {
+      ; // wait for serial port to connect. Needed for native USB port only
+    }
+  #endif
 
   // Mark program start
-  Serial.println("");
-  Serial.println(" _____     _ _        _____       _     _         ");
-  Serial.println("|  |  |___| | |___   |  _  |___ _| |_ _|_|___ ___ ");
-  Serial.println("|     | -_| | | . |  |     |  _| . | | | |   | . |");
-  Serial.println("|__|__|___|_|_|___|  |__|__|_| |___|___|_|_|_|___|");
-  Serial.println("");
+  #ifdef HAS_SERIAL
+    Serial.println("");
+    Serial.println(" _____     _ _        _____       _     _         ");
+    Serial.println("|  |  |___| | |___   |  _  |___ _| |_ _|_|___ ___ ");
+    Serial.println("|     | -_| | | . |  |     |  _| . | | | |   | . |");
+    Serial.println("|__|__|___|_|_|___|  |__|__|_| |___|___|_|_|_|___|");
+    Serial.println("");
+  #endif
 
   #ifdef HAS_ETHERNET  
     // Initialize Ethernet shield
-    Serial.println("Establishing ethernet connection...");
+    #ifdef HAS_SERIAL
+      Serial.println("Establishing ethernet connection...");
+    #endif
     // Ethernet.setSubnetMask(subnetMask);
     Ethernet.begin(mac, ip);
 
     // Check Ethernet Shield board is present
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield not found.");
+      #ifdef HAS_SERIAL
+        Serial.println("Ethernet shield not found.");
+      #endif
+      #ifdef HAS_LCD
+        printLCD("Ethernet shield", "not found - halt");
+      #endif
       while(true){;} // abort
     }
   #endif
 
   #ifdef HAS_DHT11
     dht.begin();
-    Serial.println(F("DHTxx Unified Sensor Example"));
     // Print temperature sensor details.
     sensor_t sensor;
     dht.temperature().getSensor(&sensor);
-    Serial.println(F("------------------------------------"));
-    Serial.println(F("Temperature Sensor"));
-    Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-    Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-    Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-    Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-    Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-    Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
-    Serial.println(F("------------------------------------"));
+    #ifdef HAS_SERIAL
+      Serial.println(F("------------------------------------"));
+      Serial.println(F("Temperature Sensor"));
+      Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+      Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+      Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+      Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+      Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+      Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+      Serial.println(F("------------------------------------"));
+    #endif
     // Print humidity sensor details.
     dht.humidity().getSensor(&sensor);
-    Serial.println(F("Humidity Sensor"));
-    Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-    Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-    Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-    Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-    Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-    Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
-    Serial.println(F("------------------------------------"));
+    #ifdef HAS_SERIAL
+      Serial.println(F("Humidity Sensor"));
+      Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+      Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+      Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+      Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+      Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+      Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+      Serial.println(F("------------------------------------"));
+    #endif
     // Set delay between sensor readings based on sensor details.
     delayMS = sensor.min_delay / 1000;
   #endif
@@ -170,46 +190,58 @@ void loop() {
   #ifdef HAS_WTR
     wtrVal = digitalRead(PIN_WTR);
     if(wtrVal == HIGH){
-      Serial.println("Wet!");
-      alarm(true);
-      #ifdef HAS_LCD
-        printLCD("LEAK ALERT", "Empty tank now!");
+      #ifdef HAS_SERIAL
+        Serial.println("Wet!");
       #endif
+      #ifdef HAS_LCD
+        printLCD("Leak alert!", "Empty tank now!");
+      #endif
+      alarm(true);
     } else {
-      Serial.println("Dry.");
+      #ifdef HAS_LCD
+        printLCD("No water spill", "detected");
+      #endif
+      #ifdef HAS_SERIAL
+        Serial.println("Dry.");
+      #endif
       alarm(false);
+      delay(3000);
     }
   #endif
 
-  // Read ambient temperature and humidity
+  // Read ambient temperature and humidity (skip in case of the spill)
   float ambientTemperature = 0;
   float ambientHumidity = 0;
-  #ifdef HAS_DHT11
-    // Delay between measurements.
-    delay(delayMS);  
-    sensors_event_t event;
-    dht.temperature().getEvent(&event);
-    ambientTemperature = event.temperature;
-    dht.humidity().getEvent(&event);
-    ambientHumidity = event.relative_humidity;
-    if (isnan(ambientHumidity) || isnan(ambientTemperature)) {
-      Serial.println("Failed to read from DHT sensor!");
-      return;
-    }
-  #endif
+  if (wtrVal != HIGH){
+    #ifdef HAS_DHT11
+      // Delay between measurements.
+      delay(delayMS);  
+      sensors_event_t event;
+      dht.temperature().getEvent(&event);
+      ambientTemperature = event.temperature;
+      dht.humidity().getEvent(&event);
+      ambientHumidity = event.relative_humidity;
+      if (isnan(ambientHumidity) || isnan(ambientTemperature)) {
+        #ifdef HAS_SERIAL
+          Serial.println("Failed to read from DHT sensor!");
+        #endif
+        return;
+      }
+    #endif
 
-  #ifdef HAS_LCD
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Temp: ");
-    lcd.print(ambientTemperature);
-    lcd.print("*C");
-    lcd.setCursor(0, 1);
-    lcd.print("Humi: ");
-    lcd.print(ambientHumidity);
-    lcd.print("%");
-    delay(2000);
-  #endif
+    #ifdef HAS_LCD
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Temp: ");
+      lcd.print(ambientTemperature);
+      lcd.print("*C");
+      lcd.setCursor(0, 1);
+      lcd.print("Humi: ");
+      lcd.print(ambientHumidity);
+      lcd.print("%");
+      delay(3000);
+    #endif
+  }
 
   #ifdef HAS_ETHERNET
     // Ensure Ethernet cable is connected
@@ -221,20 +253,19 @@ void loop() {
     // Read set temerature
     int on = getOnOff();
 
-    // Set temerature
-    setSetTemperature(ambientTemperature);
-
-    // Read water level
     if(wtrVal == HIGH){
+      // Leak - turn off and skip delay (alarm instant)
       if (on) setOnOff(0);
       return;
     } else {
-      setOnOff(1);
+      // No leak - turn on and set temperature
+      if (!on) setOnOff(1);
+      if (ambientTemperature != 0) setSetTemperature(ambientTemperature);
     }
   #endif
 
   // Delay
-  delay(60000);
+  delay(5000);
 }
 
 // Alarm function - LED and active buzzer
@@ -270,67 +301,99 @@ void alarm(bool state){
   // Function outputs read request status
   void printResult(int result){
     if (result == 0) {
-      Serial.println(modbusTCPClient.lastError());
+      #ifdef HAS_SERIAL
+        Serial.println(modbusTCPClient.lastError());
+      #endif
       return;
     }
-    Serial.print("Request success. Available to read/write ");
-    Serial.print(result);
-    Serial.println(" values.");
+    #ifdef HAS_SERIAL
+      Serial.print("Request success. Available to read/write ");
+      Serial.print(result);
+      Serial.println(" values.");
+    #endif
   }
 
   // Function waits for Ethernet cable to get connected
   void checkConnectEthernet(){
     if (Ethernet.linkStatus() == LinkOFF){
-      Serial.print("Ethernet cable not connected. Connect now.");
+      #ifdef HAS_SERIAL
+        Serial.print("Ethernet cable not connected. Connect now.");
+      #endif
+      #ifdef HAS_LCD
+        printLCD("Ethernet offline", "Connect cable");
+      #endif
       #ifdef HAS_LED
         digitalWrite(PIN_LED_YELLOW, HIGH);
       #endif      
     }
     while (Ethernet.linkStatus() == LinkOFF) {
-      Serial.print(".");
+      #ifdef HAS_SERIAL
+        Serial.print(".");
+      #endif
       delay(1000);
     }
     #ifdef HAS_LED
       digitalWrite(PIN_LED_YELLOW, LOW);
     #endif
-    Serial.println("Ethernet cable is connected.");
+    #ifdef HAS_SERIAL
+      Serial.println("Ethernet cable is connected.");
+    #endif
   }
 
   // Function checks id MODBUS is connected. Connects if necessary.
   void checkConnectModbus(){
     // Check if Modbus TCP client is connected
-    Serial.print("Modbus TCP client is not conected. Connecting.");  
     while(!modbusTCPClient.connected()){
+      #ifdef HAS_SERIAL
+        Serial.print("Modbus TCP client is not conected. Connecting.");  
+      #endif
+      #ifdef HAS_LCD
+        printLCD("MODBUS offline", "Reconnecting...");
+      #endif
       modbusTCPClient.stop();
       modbusTCPClient.begin(ipApiste, 502);
       // Delay 10 seconds
-      for (int i=0; i<10; i++){
+      for (int i=0; i<20; i++){
         #ifdef HAS_LED
           digitalWrite(PIN_LED_YELLOW, i%2 == 0 ? HIGH : LOW);
         #endif
-        Serial.print(".");
-        delay(1000);
+        #ifdef HAS_SERIAL
+          Serial.print(".");
+        #endif
+        delay(500);
       }
     }
-    Serial.println("\nModbus TCP client is connected.");
+    #ifdef HAS_SERIAL
+      Serial.println("\nModbus TCP client is connected.");
+    #endif
+    #ifdef HAS_LCD
+      printLCD("MODBUS online", "Connection ok");
+    #endif
+
   }
 
   double getSetTemperature(){
     // Read Set Temperature (Read Holding Register)
-    Serial.println("\nReading Setting Temperature");
+    #ifdef HAS_SERIAL
+      Serial.println("\nReading Setting Temperature");
+    #endif
     int result = modbusTCPClient.requestFrom(0x01, HOLDING_REGISTERS, 0x0002, 2); // id, type, address, nb - number of values
     printResult(result);
     for (int i=0; i < result-1; i++){
-      Serial.print((double)modbusTCPClient.read()/100, 2);
-      Serial.println(" C");
+      #ifdef HAS_SERIAL
+        Serial.print((double)modbusTCPClient.read()/100, 2);
+        Serial.println(" C");
+      #endif
     }
   }
 
   void setSetTemperature(double t){
-      // Write Set Temperature
-    Serial.print("\nWriting Set Temperature ");
-    Serial.print(t);
-    Serial.println("*C");
+    // Write Set Temperature
+    #ifdef HAS_SERIAL
+      Serial.print("\nWriting Set Temperature ");
+      Serial.print(t);
+      Serial.println("*C");
+    #endif
     int result = modbusTCPClient.beginTransmission(0x01, HOLDING_REGISTERS,  0x0002, 2); // int id, int type, int address, int nb
     printResult(result);
     modbusTCPClient.write((unsigned int)(t*100));
@@ -339,21 +402,27 @@ void alarm(bool state){
 
   int getOnOff(){
     // Read ON/OFF Status
-    Serial.println("\nReading ON/OFF Status");
+    #ifdef HAS_SERIAL
+      Serial.println("\nReading ON/OFF Status");
+    #endif
     int result = modbusTCPClient.requestFrom(0x01, HOLDING_REGISTERS, 0x2F00, 2); // id, type, address, nb - number of values
     printResult(result);
     int status = 0;
     for (int i=0; i < result-1; i++){
       status = modbusTCPClient.read();
-      Serial.println(status==1 ? "ON" : "OFF");
+      #ifdef HAS_SERIAL
+        Serial.println(status==1 ? "ON" : "OFF");
+      #endif
     }
     return status;
   }
 
   void setOnOff(int onOff){
     // Turn ON Test
-    Serial.print("\nWriting ");
-    Serial.println(onOff == 0 ? "OFF Status" : "ON Status");
+    #ifdef HAS_SERIAL
+      Serial.print("\nWriting ");
+      Serial.println(onOff == 0 ? "OFF Status" : "ON Status");
+    #endif
     int result = modbusTCPClient.beginTransmission(0x01, HOLDING_REGISTERS, 0x2F00, 2); // int id, int type, int address, int nb
     printResult(result);
     modbusTCPClient.write(onOff);
